85624adda5ef90f60ed53f5f07e8b79506a0ebc5
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-09-21 10:28:05 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 # function that prints a string in uppercase followed by a new line.
 
 def uppercase(str):
-    """Function checks for lowercase characters."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
