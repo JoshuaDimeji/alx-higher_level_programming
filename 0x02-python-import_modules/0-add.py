2753285281a8c8a63973857e579c6ec3b7707d0c
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-09-23 11:32:40 -0700

    program that imports the function def add(a, b)

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# program that imports the function def add(a, b):
+
+from add_0 import add
+a = 1
+b = 2
+if __name__ == "__main__":
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
