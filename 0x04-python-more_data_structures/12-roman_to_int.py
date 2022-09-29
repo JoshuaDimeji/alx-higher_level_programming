0240ea87521785659ced5b8854738162f4ebecd7
Joshua Ogungbenro <ogungbenrojoshua2000@gmail.com>
2022-09-29 15:19:35 -0700

    function that converts Roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return None
-    n = 0
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for i in roman_string:
-        n += roman.get(i, 0)
-    subs = {'CM': -200, 'CD': -200, 'XC': -20, 'XL': -20, 'IX': -2, 'IV': -2}
-    chars = ""
-    for i in range(0, len(roman_string) - 1):
-        chars = roman_string[i] + roman_string[i + 1]
-        if chars in subs:
-            n += subs[chars]
-    return n
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    i = 0
+
+    if (roman_string is None) or (type(roman_string) is not str):
+        return total
+
+    while i in range(len(roman_string)):
+
+        if i == len(roman_string) - 1:
+            total += roman[roman_string[i]]
+            break
+
+        else:
+            if roman[roman_string[i]] < roman[roman_string[i+1]]:
+                total += roman[roman_string[i+1]] - roman[roman_string[i]]
+                i = i + 1
+
+                if i + 1 == len(roman_string):
+                    break
+
+            else:
+                total += roman[roman_string[i]]
+
+        i = i + 1
+
+    return total
